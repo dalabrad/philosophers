bc4bbdce645386055d4c6b9651c7ea77f4b8b57d
David <dalabrad@student.42urduliz.com>
2025-02-10 18:08:44 +0100

    modified main.c

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,13 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 11:05:20 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/02/10 17:40:44 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/02/10 18:08:06 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/philosophers.h"
 
-int	main(int argc, char *argv[])
+/* int	main(int argc, char *argv[])
 {
 	t_data	philo;
 
@@ -37,4 +37,25 @@ int	main(int argc, char *argv[])
 			printf("N times philo must eat (opt) : %zu\n", philo.n_limit_meals);
 	}
 	return (EXIT_SUCCESS);
+}
+ */
+
+int	main(int argc, char **argv)
+{
+	t_data	table;
+	
+	if (argc == 5 || argc == 6)
+	{
+		printf("Here the main program %s should start!!\n", argv[0]);
+/* 		//1) Parse from argv => check input format error and fill the data table.
+		parse_input(&table);
+		//2) Create the threads(philosophers) / mutex (forks), get the time ...
+		data_init(&table);
+		//3) Start the simulation
+		feast_start(&table);
+		//4) Free all the allocated data (NO LEAKS!!!)
+		clean(&table); */
+	}
+	else
+		error_exit(INPUT_ERROR, NULL);
 }
