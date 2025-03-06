7bdad5a8c6275567d9d3fc4a181652f431ccd6d5
David Labrador Gracia <dalabrad@c4r6s5.42urduliz.com>
2025-03-06 16:21:33 +0100

    eliminated formatted output for philo_write_utils.c

--- philo/src/philo_write_utils.c
+++ philo/src/philo_write_utils.c
@@ -6,7 +6,7 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 18:14:34 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/03/03 17:05:55 by dalabrad         ###   ########.fr       */
+/*   Updated: 2025/03/06 15:24:14 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,8 @@ void	write_status(t_philo_status status, t_philo *philo, bool debug)
 	long	elapsed;
 
 	elapsed = get_time(MILISECOND) - philo->data->start_simulation;
-	if (get_bool(&philo->philo_mutex, &philo->full))
-		return ;
+/* 	if (get_bool(&philo->philo_mutex, &philo->full))
+		return ; */
 	safe_mutex_handle(&(philo->data->write_mutex), LOCK);
 	if (debug)
 		write_status_debug(status, philo, elapsed);
@@ -54,15 +54,15 @@ void	write_status(t_philo_status status, t_philo *philo, bool debug)
 	{
 		if ((status == TAKE_FIRST_FORK || status == TAKE_SECOND_FORK)
 			&& !simulation_finished(philo->data))
-			printf(W "%ld" RST " %d has taken a fork\n", elapsed, philo->id);
+			printf("%ld %d has taken a fork\n", elapsed, philo->id);
 		else if (status == EATING && !simulation_finished(philo->data))
-			printf(W "%ld" C " %d is eating\n"RST, elapsed, philo->id);
+			printf("%ld %d is eating\n", elapsed, philo->id);
 		else if (status == SLEEPING && !simulation_finished(philo->data))
-			printf(W "%ld" G " %d is sleeping\n" RST, elapsed, philo->id);
+			printf("%ld %d is sleeping\n", elapsed, philo->id);
 		else if (status == THINKING && !simulation_finished(philo->data))
-			printf(W "%ld" Y " %d is thinking\n" RST, elapsed, philo->id);
+			printf("%ld %d is thinking\n", elapsed, philo->id);
 		else if (status == DIED)
-			printf(R "%ld %d died\n" RST, elapsed, philo->id);
+			printf("%ld %d died\n", elapsed, philo->id);
 	}
 	safe_mutex_handle(&(philo->data->write_mutex), UNLOCK);
 }
